d263ffa5440c668fe4203abece157969b080a57a
Michael Mitchell <awildbeard@tuta.io>
2019-09-26 11:09:25 -0500

    Added connection test method

--- server.py
+++ server.py
@@ -6,7 +6,11 @@ from time import sleep
 
 class ExchangeServicer(exchange_pb2_grpc.ExchangeServicer):
     def GetData(self, request, context):
+        print("Recieved gRPC GetData request")
         return exchange_pb2.DataResponse(response="100")
+    def GRPCHello(self, request, context):
+        print(f"Recieved gRPC Hello: {request}")
+        return exchange_pb2.HelloResponse(hello="connected")
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
